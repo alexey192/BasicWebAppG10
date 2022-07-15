8eaf80f459d18778635325223c3b8eb6c5018528
alexeybednik <Kv6XWWqGybnhX3>
2022-07-15 10:40:12 +0200

    largest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,8 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,8 +27,20 @@ public class QueryProcessor {
             var int2 = Integer.parseInt(m.group());
             return String.valueOf(int1+int2);
 
-        } else if (query.contains("name")) {
-            return "Alex";
+        } else if (query.contains("the largest")) {
+            Pattern p = Pattern.compile("-?\\d+");
+            Matcher m = p.matcher(query);
+
+            var t = new ArrayList<Integer>();
+            while (m.find()) {
+                t.add(Integer.parseInt(m.group()));
+            }
+
+            int maxx = t.stream()
+                    .mapToInt(v -> v)
+                    .max().orElseThrow(NoSuchElementException::new);
+
+            return String.valueOf(maxx);
 
         } else if (query.contains("name")) {
             return "Alex";
